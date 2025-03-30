7135dba00990268ece3646129d74d749728285d1
Jicolussi <joaquincolussipuig@gmail.com>
2025-03-29 21:43:22 -0300

    agrege archivos del tp2 del punto 1 al 10

---
+++ digitalesIII/Tp2_DiIII/Punto_3/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#include "stm32f103xb.h"
+
+void setup() {
+  RCC->APB2ENR|=RCC_APB2ENR_IOPAEN_Msk;
+  RCC->APB2ENR|=RCC_APB2ENR_IOPBEN_Msk;
+  GPIOB->CRL=(0x11111111);
+  GPIOB->CRH=(0x11111111);
+  GPIOA->CRL=(0x44444444);
+  GPIOA->CRH=(0x44444444);
+}
+
+void loop() {
+  uint16_t est=GPIOB->IDR;
+  GPIOA->BSRR=(~est+1u);
+}
