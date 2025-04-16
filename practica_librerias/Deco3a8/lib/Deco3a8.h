a2ef3f7b26051a78c4fa3fb245233566da7659a8
jicolussi <joaquincolussipuig@gmail.com>
2025-04-16 16:57:46 -0300

    punto 1 16_4

---
+++ practica_librerias/Deco3a8/lib/Deco3a8.h
@@ -0,0 +1,8 @@
+#include <Arduino.h>
+#ifdef DECO3A8_H
+#define DECO3A8_h
+
+void dec3a8_init();
+void dec3a8(int I0; int I1, int I2);
+
+#endif
---
+++ practica_librerias/Deco3a8/src/Deco3a8.h
@@ -0,0 +1,6 @@
+#include <Arduino.h>
+#ifdef DECO3A8_H
+#define DECO3A8_h
+void (int pin)
+
+
