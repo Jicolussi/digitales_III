a78329b791d6a9b2e260a12729f4c1bad3c35746
jicolussi <joaquincolussipuig@gmail.com>
2025-04-23 16:47:46 -0300

    prueba7seg

--- practica_librerias/7segmentos/lib/7seg/dec7seg.h
+++ practica_librerias/7segmentos/lib/7seg/dec7seg.h
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #ifdef DEC7SEG_H
-#define 7SEG_H
+#define DEC7SEG_H
 
 void dec7seg_init();
 void dec7seg(int num);
--- practica_librerias/7segmentos/src/main.cpp
+++ practica_librerias/7segmentos/src/main.cpp
@@ -1,38 +1,11 @@
-<<<<<<< HEAD
-#include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include <arduino.h>
+#include "dec7seg.h"
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  void dec7seg_init();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-=======
-#include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
->>>>>>> 21772e1a49ddf5ab8b4d32de07f7144211c24b8c
+  dec7seg(0);
+  delay(1000);
 }
